603e18ca592a4ce2c409c777c1ee0689cf189a17
Viet Le <vietlq85@gmail.com>
2017-07-30 18:39:32 +0100

    indentation

--- lexer.ml
+++ lexer.ml
@@ -7,34 +7,34 @@ let rec lex stream =
   (* The end of stream *)
   | None -> None
   | Some c -> begin
-    match c with
-    (* Ignore white spaces *)
-    | ' ' | '\t' | '\n' | '\r' -> Stream.junk stream ; lex stream
-    (* Important characters *)
-    | '{' -> ret_val Token.T_OBJ_BEGIN stream
-    | '}' -> ret_val Token.T_OBJ_END stream
-    | '[' -> ret_val Token.T_ARR_BEGIN stream
-    | ']' -> ret_val Token.T_ARR_END stream
-    | ':' -> ret_val Token.T_COLON stream
-    | ',' -> ret_val Token.T_COMMA stream
-    (* Lex a string *)
-    | '"' ->
-      let buffer = Buffer.create 1 in
-      Stream.junk stream ;
-      lex_string buffer stream
-    (* Lex a number *)
-    | '-' | '0'..'9' as c ->
-      let buffer = Buffer.create 1 in
-      Stream.junk stream ;
-      Buffer.add_char buffer c ;
-      lex_number buffer stream
-    (* Lex true/false/null *)
-    | 't' | 'f' | 'n' as c ->
-      let buffer = Buffer.create 1 in
-      Stream.junk stream ;
-      Buffer.add_char buffer c ;
-      lex_tfn buffer stream
-    | c -> failwith (Printf.sprintf "Invalid character: %c" c)
+      match c with
+      (* Ignore white spaces *)
+      | ' ' | '\t' | '\n' | '\r' -> Stream.junk stream ; lex stream
+      (* Important characters *)
+      | '{' -> ret_val Token.T_OBJ_BEGIN stream
+      | '}' -> ret_val Token.T_OBJ_END stream
+      | '[' -> ret_val Token.T_ARR_BEGIN stream
+      | ']' -> ret_val Token.T_ARR_END stream
+      | ':' -> ret_val Token.T_COLON stream
+      | ',' -> ret_val Token.T_COMMA stream
+      (* Lex a string *)
+      | '"' ->
+        let buffer = Buffer.create 1 in
+        Stream.junk stream ;
+        lex_string buffer stream
+      (* Lex a number *)
+      | '-' | '0'..'9' as c ->
+        let buffer = Buffer.create 1 in
+        Stream.junk stream ;
+        Buffer.add_char buffer c ;
+        lex_number buffer stream
+      (* Lex true/false/null *)
+      | 't' | 'f' | 'n' as c ->
+        let buffer = Buffer.create 1 in
+        Stream.junk stream ;
+        Buffer.add_char buffer c ;
+        lex_tfn buffer stream
+      | c -> failwith (Printf.sprintf "Invalid character: %c" c)
     end
 
 and lex_string buffer stream =
