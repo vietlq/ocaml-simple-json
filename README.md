873a01e2f77c7556a2ce5613b945510f17a06c63
Viet Le <vietlq85@gmail.com>
2017-07-30 19:43:35 +0000

    profiling

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 https://opam.ocaml.org/doc/Packaging.html
 https://github.com/ocaml/opam-repository
 
+https://ocaml.org/learn/tutorials/performance_and_profiling.html
+
 http://numeraljs.com/
 http://www.json-generator.com/
 https://www.npmjs.com/package/faker
--- json/run_json_test.sh
+++ json/run_json_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
-ocamlopt -O2 -o test_json_parser json.ml lexer.ml parser.ml test_json_parser.ml \
+ocamlopt -p -ccopt -O2 -o test_json_parser json.ml lexer.ml parser.ml test_json_parser.ml \
     && time gunzip < test_medium.json.gz | ./test_json_parser
