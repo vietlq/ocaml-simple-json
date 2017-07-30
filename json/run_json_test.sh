ebe821937e368617755c9793bbe465089f0ff913
Viet Le <vietlq85@gmail.com>
2017-07-30 20:47:56 +0000

    compiler flag for profiling

--- json/run_json_test.sh
+++ json/run_json_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
-ocamlopt -p -ccopt -O2 -o test_json_parser json.ml lexer.ml parser.ml test_json_parser.ml \
+ocamlopt -p -ccopt -O2 -ccopt -fno-omit-frame-pointer -o test_json_parser \
+    json.ml lexer.ml parser.ml test_json_parser.ml \
     && time gunzip < test_medium.json.gz | ./test_json_parser
