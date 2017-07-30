98aadbb17b6a07d6589cb3253f2b16b46e6fe26d
Viet Le <vietlq85@gmail.com>
2017-07-30 22:18:26 +0100

    Fix bug in parse_file

--- json/time_test_json.ml
+++ json/time_test_json.ml
@@ -2,24 +2,33 @@ let parse_in_chan in_chan () =
   Parser.parse_top_level @@ Lexer.lex @@ Stream.of_channel @@ in_chan
 
 let parse_file file () =
-  parse_in_chan @@ open_in file
+  parse_in_chan (open_in file) ()
 
 let time f =
   let t = Sys.time () in
-  let _ = f () in
-  Printf.printf "\nExecution time: %fs\n" (Sys.time() -. t)
+  let res = f () in
+  Printf.printf "\nExecution time: %fs\n" (Sys.time() -. t) ;
+  res
 
 let usage () =
   Printf.printf "1) Run: %s < input.json\n" Sys.argv.(0) ;
   Printf.printf "2) Run: %s input.json\n" Sys.argv.(0)
 
+let print_res res () =
+  match res with
+  | None -> print_endline "Got None"
+  | Some (Parser.Ok json, _) -> print_endline @@ Json.string_of_json json
+  | Some (Parser.Error e, _) -> Printf.printf "Got error: %s\n" e
+
 let () =
   match Array.length Sys.argv with
   | 1 ->
     Printf.printf "Parsing JSON from STDIN\n";
-    time @@ parse_in_chan stdin
+    (* parse_in_chan stdin |> time |> print_res |> time |> ignore *)
+    parse_in_chan stdin |> time |> ignore
   | 2 ->
     let file = Sys.argv.(1) in
     Printf.printf "Parsing JSON from the file %s\n" file;
-    time @@ parse_file file
+    (* parse_file file |> time |> print_res |> time |> ignore *)
+    parse_file file |> time |> ignore
   | _ -> usage ()
