fde26f4bf175821eec3568bfc6eb1867f79dcf9b
Viet Le <vietlq85@gmail.com>
2017-07-30 01:53:55 +0100

    New stream lexer

--- simple_lexer.ml
+++ simple_lexer.ml
@@ -12,23 +12,32 @@ type token =
   | T_NULL
 ;;
 
-let rec lex stream =
-  match Stream.peek stream with
-  (* The end of stream *)
-  | None -> Stream.from (fun n -> Printf.printf "Count = %d" n ; None)
-  | Some c -> begin
-    match c with
-    (* Ignore white spaces *)
-    | ' ' | '\t' | '\n' | '\r' -> Stream.junk stream; lex stream
-    (* Important characters *)
-    | '{' -> Stream.from (fun n -> Printf.printf "Count = %d" n ; Some T_OBJ_BEGIN)
-    | '}' -> Stream.from (fun n -> Printf.printf "Count = %d" n ; Some T_OBJ_END)
-    | '[' -> Stream.from (fun n -> Printf.printf "Count = %d" n ; Some T_ARR_BEGIN)
-    | ']' -> Stream.from (fun n -> Printf.printf "Count = %d" n ; Some T_ARR_END)
-    | ':' -> Stream.from (fun n -> Printf.printf "Count = %d" n ; Some T_COLON)
-    | ',' -> Stream.from (fun n -> Printf.printf "Count = %d" n ; Some T_COMMA)
-    | _ -> Stream.junk stream; lex stream
-  end
+let make_lazy_list t stream = LazyList (t, fun () -> lex stream) ;;
+
+let stream_of_token t = Stream.from (fun acc -> Some t) ;;
+
+let ret_val t stream = (stream_of_token t, stream) ;;
+
+let lex stream =
+  let rec aux stream acc =
+    match Stream.peek stream with
+    (* The end of stream *)
+    | None -> (Stream.from (fun acc -> None), stream)
+    | Some c -> begin
+      Stream.junk stream;
+      match c with
+      (* Ignore white spaces *)
+      | ' ' | '\t' | '\n' | '\r' -> aux stream (acc+1)
+      (* Important characters *)
+      | '{' -> ret_val T_OBJ_BEGIN stream
+      | '}' -> ret_val T_OBJ_END stream
+      | '[' -> ret_val T_ARR_BEGIN stream
+      | ']' -> ret_val T_ARR_END stream
+      | ':' -> ret_val T_COLON stream
+      | ',' -> ret_val T_COMMA stream
+      | _ -> aux stream (acc+1)
+    end
+  in aux stream 0
 ;;
 
 let in_chan = Stream.of_channel stdin ;;
