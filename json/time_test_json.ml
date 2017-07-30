427e136341f494067ec996e8a6f1a435437f8aa4
Viet Le <vietlq85@gmail.com>
2017-07-30 22:55:57 +0100

    More user friendly with Arg.parse

--- json/time_test_json.ml
+++ json/time_test_json.ml
@@ -10,9 +10,17 @@ let time f =
   Printf.printf "\nExecution time: %fs\n" (Sys.time() -. t) ;
   res
 
-let usage () =
-  Printf.printf "1) Run: %s < input.json\n" Sys.argv.(0) ;
-  Printf.printf "2) Run: %s input.json\n" Sys.argv.(0)
+let verbose = ref false
+let file_name = ref ""
+let ask_help = ref false
+let speclist = [
+  ("-v", Arg.Set verbose, "Prints JSON after parsing");
+  ("-f", Arg.Set_string file_name, "JSON file name. Omit for STDIN");
+  ("-h", Arg.Set ask_help, "Print help")
+]
+
+let usage_msg =
+  "Parse a valid JSON file. Available options:"
 
 let print_res res () =
   match res with
@@ -20,15 +28,23 @@ let print_res res () =
   | Some (Parser.Ok json, _) -> print_endline @@ Json.string_of_json json
   | Some (Parser.Error e, _) -> Printf.printf "Got error: %s\n" e
 
-let () =
-  match Array.length Sys.argv with
-  | 1 ->
+let main_helper () =
+  match !file_name with
+  | "" ->
     Printf.printf "Parsing JSON from STDIN\n";
-    (* parse_in_chan stdin |> time |> print_res |> time |> ignore *)
-    parse_in_chan stdin |> time |> ignore
-  | 2 ->
-    let file = Sys.argv.(1) in
+    if !verbose
+    then parse_in_chan stdin |> time |> print_res |> time |> ignore
+    else parse_in_chan stdin |> time |> ignore
+  | file ->
     Printf.printf "Parsing JSON from the file %s\n" file;
-    (* parse_file file |> time |> print_res |> time |> ignore *)
-    parse_file file |> time |> ignore
-  | _ -> usage ()
+    if !verbose
+    then parse_file file |> time |> print_res |> time |> ignore
+    else parse_file file |> time |> ignore
+
+let () =
+  Arg.parse speclist
+    (fun anon -> file_name := anon)
+    usage_msg ;
+  if !ask_help
+  then Arg.usage speclist usage_msg
+  else main_helper ()
