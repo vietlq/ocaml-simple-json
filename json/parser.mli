98e4d48e5bf1eccd1123082f7e7a12299f46effb
Viet Le <vietlq85@gmail.com>
2017-08-02 07:49:21 +0100

    trimmed API

--- json/parser.mli
+++ json/parser.mli
@@ -1,31 +1,7 @@
 type ('a, 'b) result = Ok of 'a | Error of 'b
-val parse_value :
-  (Token.token * char Stream.t) option ->
-  ((Json.value, string) result * char Stream.t) option
-val parse_json_string :
-  (Token.token * char Stream.t) option ->
-  ((Json.value, string) result * char Stream.t) option
-val parse_json_number :
-  (Token.token * char Stream.t) option ->
-  ((Json.value, string) result * char Stream.t) option
-val parse_json_bool :
-  (Token.token * char Stream.t) option ->
-  ((Json.value, string) result * char Stream.t) option
-val parse_json_null :
-  (Token.token * char Stream.t) option ->
-  ((Json.value, string) result * char Stream.t) option
-val parse_primary :
-  (Token.token * char Stream.t) option ->
-  ((Json.value, string) result * char Stream.t) option
-val parse_array :
-  (Token.token * char Stream.t) option ->
-  ((Json.value, string) result * char Stream.t) option
-val parse_object_pair :
-  (Token.token * char Stream.t) option ->
-  (string * Json.value) * char Stream.t
-val parse_object :
-  (Token.token * char Stream.t) option ->
-  ((Json.value, string) result * char Stream.t) option
+(** Return variant type: Ok for good operations and Error for errors *)
+
 val parse_top_level :
   (Token.token * char Stream.t) option ->
   ((Json.value, string) result * char Stream.t) option
+(** Parse top level JSON values from tokens: Array or Object *)
